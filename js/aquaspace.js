13f7b0e61118bbb8a4f072cd124fb56e2df90845
andreastkdf <andreaskostakis@gmail.com>
2018-01-24 00:59:01 +0100

    page loading

--- js/aquaspace.js
+++ js/aquaspace.js
@@ -1,3 +1,39 @@
+// loading
+;(function(){
+  function id(v){ return document.getElementById(v); }
+  function loadbar() {
+    var ovrl = id("overlay"),
+        prog = id("progress"),
+        stat = id("progstat"),
+        img = document.images,
+        c = 0,
+        tot = img.length;
+    if(tot == 0) return doneLoading();
+
+    function imgLoaded(){
+      c += 1;
+      var perc = ((100/tot*c) << 0) +"%";
+      prog.style.width = perc;
+      stat.innerHTML = perc;
+      if(c===tot) return doneLoading();
+    }
+    function doneLoading(){
+      ovrl.style.opacity = 0;
+      setTimeout(function(){ 
+        ovrl.style.display = "none";
+      }, 1200);
+    }
+    for(var i=0; i<tot; i++) {
+      var tImg     = new Image();
+      tImg.onload  = imgLoaded;
+      tImg.onerror = imgLoaded;
+      tImg.src     = img[i].src;
+    }    
+  }
+  document.addEventListener('DOMContentLoaded', loadbar, false);
+}());
+
+
 $("#logo").click(function(){
 	$("#logo").fadeOut('300');
 	$("#logo_small").fadeIn('200');
@@ -61,7 +97,6 @@ $('.carousel').on('slid.bs.carousel', function (evt) {
 		$('#textSlide'+step).show('slow');
 	});
 });
-
 // var img = $("#starRating img");
 // img.attr("src", img.attr("src").replace("yelp", "my"));
 
