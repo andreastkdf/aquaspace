5a4089ec73590b4350dbd1d46c7609ee98067ecd
andreastkdf <andreaskostakis@gmail.com>
2018-03-26 00:21:21 +0200

    aqua preparing last version 4

--- js/aquaspace.js
+++ js/aquaspace.js
@@ -144,8 +144,10 @@ $('#udato_container').click(function () {
 
 	if ((show == 0) && (next>1)){
 		$(".bam").attr("class","ml11");
-		$(".letters").fadeIn('fast');
-		$(".line").fadeIn('fast');
+		// $(".letters").fadeIn('fast');
+		// $(".line").fadeIn('slow');
+		$(".line").toggle('fast');
+		$(".letters").toggle(2500);
 		$(".Tokyo").html('υδατοκαλλι&#941;ργεια');
 		show = 1;
 	}
@@ -566,37 +568,3 @@ function changeImageChicken(){
 
 
 // Wrap every letter in a span
-$('.ml11 .letters').each(function(){
-	$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-});
-
-anime.timeline({loop: true})
-.add({
-	targets: '.ml11 .line',
-	scaleY: [0,1],
-	opacity: [0.5,1],
-	easing: "easeOutExpo",
-	duration: 700
-})
-.add({
-	targets: '.ml11 .line',
-	translateX: [0,$(".ml11 .letters").width()],
-	easing: "easeOutExpo",
-	duration: 700,
-	delay: 100
-}).add({
-	targets: '.ml11 .letter',
-	opacity: [0,1],
-	easing: "easeOutExpo",
-	duration: 600,
-	offset: '-=775',
-	delay: function(el, i) {
-		return 34 * (i+1)
-	}
-}).add({
-	targets: '.ml11',
-	opacity: 0,
-	duration: 1000,
-	easing: "easeOutExpo",
-	delay: 1000
-});
